2badf13c865221b5662765485f781d3c0bee0552
Maxim Rubchinsky <maxim@rubchinsky.com>
2017-03-05 22:08:03 +0200

    FreshMvvm.IOC.Common

---
+++ Xamarin/FreshMvvm.IOC.Common/AppInitializer.cs
@@ -0,0 +1,42 @@
+﻿namespace FreshMvvm.IOC.Common
+{
+    public abstract class AppInitializer
+    {
+        private readonly PlatformInitializer r_PlatformInitializer;
+
+        protected AppInitializer(PlatformInitializer i_PlatformInitializer)
+        {
+            r_PlatformInitializer = i_PlatformInitializer;
+            i_PlatformInitializer.Init();
+        }
+
+        protected IFreshIOC Container
+        {
+            get
+            {
+                return FreshIOC.Container;
+            }
+        }
+
+        public void Init()
+        {
+            r_PlatformInitializer.Init();
+            ConfigureServices();
+            Register();
+        }
+
+        /// <summary>
+        /// Configure Framework services
+        /// </summary>
+        protected virtual void ConfigureServices()
+        {
+        }
+
+        /// <summary>
+        /// Register Components using FreshIOC provider
+        /// </summary>
+        protected virtual void Register()
+        {
+        }
+    }
+}
---
+++ Xamarin/FreshMvvm.IOC.Common/ModuleRegistrationExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace FreshMvvm.IOC.Common
+{
+    public static class ModuleRegistrationExtensions
+    {
+        /// <summary>
+        /// Add a module to the container.
+        /// </summary>
+        /// <param name="i_Provider">The FreshIOC provider to register the module with.</param>
+        /// <typeparam name="TModule">The module to add.</typeparam>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="i_Provider"/> is <see langword="null"/>.
+        /// </exception>
+        public static void RegisterModule<TModule>(this IFreshIOC i_Provider) where TModule : Module, new()
+        {
+            if(i_Provider == null)
+            {
+                throw new ArgumentNullException(nameof(i_Provider));
+            }
+
+            TModule module = new TModule();
+            module.Load(i_Provider);
+        }
+
+        /// <summary>
+        /// Registers modules found in an assembly.
+        /// </summary>
+        /// <param name="i_Provider">The FreshIOC provider to register the module with.</param>
+        /// <param name="i_Assemblies">The assemblies from which to register modules.</param>
+        /// <exception cref="ArgumentNullException">
+        // Thrown if <paramref name="i_Provider"/> is <see langword="null"/>.
+        /// </exception>
+        public static void RegisterAssemblyModules(this IFreshIOC i_Provider, params Assembly[] i_Assemblies)
+        {
+            TypeInfo moduleTypeInfo = typeof(Module).GetTypeInfo();
+
+            if (i_Provider == null)
+            {
+                throw new ArgumentNullException(nameof(i_Provider));
+            }
+
+            foreach(Assembly assembly in i_Assemblies)
+            {
+                IEnumerable<Type> modules =
+                    assembly.GetExportedTypes()
+                        .Where(
+                            i_Type =>
+                                moduleTypeInfo.IsAssignableFrom(i_Type.GetTypeInfo()) && hasDefaultConstructor(i_Type));
+                foreach(Type moduleType in modules)
+                {
+                    Module module = Activator.CreateInstance(moduleType) as Module;
+                    if(module != null)
+                    {
+                        module.Load(i_Provider);
+                    }
+                }
+            }
+        }
+
+        private static bool hasDefaultConstructor(Type i_Type)
+        {
+            return i_Type.GetTypeInfo().GetConstructor(Type.EmptyTypes) != null;
+        }
+    }
+}
