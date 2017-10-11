01b51b39361f0f7a0c9ad16e7efc3734889e923a
Maxim Rubchinsky <maxim@rubchinsky.com>
2017-10-11 12:47:24 +0300

    nuget libs

--- Xamarin/Xamarin.FileStorage.iOS/Properties/AssemblyInfo.cs
+++ Xamarin/Xamarin.FileStorage.iOS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0063b0f9-6b78-4c9a-b4ae-9f17578e259d")]
+[assembly: Guid("d6e590c6-e374-45cf-96d0-a6b057be8b3c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
